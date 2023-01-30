e4026031a3a8c9250acd0147ef0a2fa6188e5b6b
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-01-30 14:03:32 -0700

    fetch link working

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const form = document.getElementById("makeup-form")
     form.addEventListener("submit", (event) => {
         event.preventDefault()
-        fetch (`https://makeup-api.herokuapp.com/api/v1/products.json${event.target.search.value}`)
-        .then((response) => response.text())
-        .then((data) => console.log(data));
+        fetch (`https://makeup-api.herokuapp.com/api/v1/products.json?q=${event.target[0].value}`)
+        .then(response => response.json())
+        .then(response => console.log("response", response))
     })
 })
+// let fetchData = [])
+// document.addEventListener("DOMContentLoaded", () =>{
+//     const form = document.getElementById("makeup-form")
+//     form.addEventListener("submit", (event) => {
+//         event.preventDefault() 
+//         console.log(event.target[0].value)
+//         fetch (`https://makeup-api.herokuapp.com/api/v1/products.json`)
+//         .then((response) => response.json())
+//        .then((data) => {
+//        fetchData = data
+//        fetchData.filter(object => console.log(object = `${event.target.search.value}`))
+//        })
+//     })
+// })
