2102d4e37520b53bdcc90c60d02718d70895ec5e
Cosette Tshibanda <cosettetshibanda@gmail.com>
2023-01-30 17:13:43 -0700

    beginner css

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    const form = document.getElementById("makeup-form")
-    form.addEventListener("submit", (event) => {
-        event.preventDefault()
-        fetch (`https://makeup-api.herokuapp.com/api/v1/products.json?q=${event.target[0].value}`)
-        .then(response => response.json())
-        .then(response => console.log("response", response))
-    })
-})
-// let fetchData = [])
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     const form = document.getElementById("makeup-form")
+//     form.addEventListener("submit", (event) => {
+//         event.preventDefault()
+//         fetch (`https://makeup-api.herokuapp.com/api/v1/products.json?q=${event.target[0].value}`)
+//         .then(response => response.json())
+//         .then(response => {
+//             response.filter(object => console.log(object === `${event.target.search.value}`))
+//         })
+//     })
+// })
+// let fetchData = []
 // document.addEventListener("DOMContentLoaded", () =>{
+//     let fetchData = []
 //     const form = document.getElementById("makeup-form")
 //     form.addEventListener("submit", (event) => {
 //         event.preventDefault() 
 //         console.log(event.target[0].value)
-//         fetch (`https://makeup-api.herokuapp.com/api/v1/products.json`)
+//         fetch (`https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline`)
 //         .then((response) => response.json())
 //        .then((data) => {
 //        fetchData = data
 //        fetchData.filter(object => console.log(object = `${event.target.search.value}`))
+//        return []
 //        })
 //     })
 // })
+
+
+const cardTemplate = document.querySelector("[data-card-template]")
+const cardContainer = document.querySelector("[data-cards-container]")
+const search = document.querySelector("[data-search]")
+
+let users = []
+
+search.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase()
+    users.forEach(user => {
+        const isVisible = user.brand.toLowerCase().includes(value) || user.name.toLowerCase().includes(value)
+        user.element.classList.toggle("hide", !isVisible)
+    })
+})
+
+fetch (`https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline`)
+         .then((response) => response.json())
+         .then((data) => {
+           users = data.map(user =>{
+                const card = cardTemplate.content.cloneNode(true).children[0]
+                const header = card.querySelector("[data-header]")
+                const body = card.querySelector("[data-body]")
+                header.textContent = user.brand
+                body.textContent = user.name
+                console.log("user", user)
+                cardContainer.append(card)
+                return {brand: user.brand, name: user.name, element: card}
+            })
+         })
